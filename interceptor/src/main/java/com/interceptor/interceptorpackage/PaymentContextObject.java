10d3fecf8eb00dd58049a446e4242583ff964e2f
Luke Bogdanovic <19232691@studentmail.ul.ie>
2023-02-24 00:58:44 +0000

    Removal of unnecessary data field and accessor

--- interceptor/src/main/java/com/interceptor/interceptorpackage/PaymentContextObject.java
+++ interceptor/src/main/java/com/interceptor/interceptorpackage/PaymentContextObject.java
@@ -4,27 +4,20 @@ import java.util.List;
 
 import com.interceptor.interceptorpackage.interfaces.IContext;
 import com.interceptor.interceptorpackage.interfaces.Interceptor;
-import com.interceptor.movierental.Customer;
 import com.interceptor.movierental.Rental;
 
 public class PaymentContextObject implements IContext {
 
-    private Customer customer;
     private List<Rental> rentals;
     private double amount;
 
-    public PaymentContextObject(Customer movie, List<Rental> rentals, double amount) {
-        this.customer = movie;
+    public PaymentContextObject(List<Rental> rentals, double amount) {
         this.rentals = rentals;
         this.amount = amount;
     }
 
     public double getAmount() {
         return amount;
-    }
-
-    public Customer getCustomer() {
-        return customer;
     }
 
     public List<Rental> getRentals() {
--- interceptor/src/main/java/com/interceptor/movierental/Customer.java
+++ interceptor/src/main/java/com/interceptor/movierental/Customer.java
@@ -48,7 +48,7 @@ public class Customer {
         }
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-        dispatcher.dispatchClientRequestInterceptor(new PaymentContextObject(this, _rentals, getTotalCharge()));
+        dispatcher.dispatchClientRequestInterceptor(new PaymentContextObject(_rentals, getTotalCharge()));
         return result;
     }
 
